f65762a3e38e1beaeef936d296caa4a351941857
Paul Davis <paul@growcreate.co.uk>
2020-07-30 11:53:52 +0100

    Updated to work with IIS Express

--- src/CustomUrls.Core/Features/UrlHandling/ContentFinder.cs
+++ src/CustomUrls.Core/Features/UrlHandling/ContentFinder.cs
@@ -2,6 +2,6 @@
 {
     public class ContentFinder : VortoUrlSegments.VortoUrlContentFinder
     {
-        override 
     }
+
 }
--- src/CustomUrls.Core/Features/UrlHandling/UrlProvider.cs
+++ src/CustomUrls.Core/Features/UrlHandling/UrlProvider.cs
@@ -1,9 +1,7 @@
-﻿using System.Web.Mvc;
-
-namespace CustomUrls.Core.Features.UrlHandling
+﻿namespace CustomUrls.Core.Features.UrlHandling
 {
     public class UrlProvider : VortoUrlSegments.VortoUrlProvider
     {
-override 
     }
+
 }
